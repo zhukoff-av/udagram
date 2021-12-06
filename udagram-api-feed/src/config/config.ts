da9cc55074c283d40f61d93ea46044a6d938a539
Anton Zhukov <anton.zhukov@immowelt.de>
2021-12-06 20:43:31 +0100

    add project files

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,21 @@
+export const config = {
+    'username': 'classdb',
+    'password': '159357159',
+    'database': 'postgres',
+    'host': 'classdb.caucrxsqr8sj.us-east-1.rds.amazonaws.com',
+    'dialect': 'postgres',
+    'aws_media_bucket': process.env.AWS_BUCKET,
+    'url': process.env.URL,
+    // 'username': process.env.POSTGRES_USERNAME,
+    // 'password': process.env.POSTGRES_PASSWORD,
+    // 'database': process.env.POSTGRES_DB,
+    // 'host': process.env.POSTGRES_HOST,
+    // 'dialect': 'postgres',
+    // 'aws_region': process.env.AWS_REGION,
+    // 'aws_profile': process.env.AWS_PROFILE,
+    // 'aws_media_bucket': process.env.AWS_BUCKET,
+    // 'url': process.env.URL,
+    'jwt': {
+        'secret': process.env.JWT_SECRET,
+    },
+};
